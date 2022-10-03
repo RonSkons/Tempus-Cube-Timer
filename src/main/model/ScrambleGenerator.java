e42ab95b8d7171a5e4a0a6116d48499561492b7f
Ron Friedman <ron852719@icloud.com>
2022-10-03 15:33:00 -0700

    - Implemented and tested cube scrambler ScrambleGenerator

---
+++ src/main/model/ScrambleGenerator.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+// Used to generate random Rubik's Cube scrambles of variable lengths
+public class ScrambleGenerator {
+    /* ASIDE: CUBE NOTATION.
+     Each letter represents a 90-degree clockwise turn of a cube face.
+     R = right, L = Left, F = front, B = back, U = up, D = down
+     R' (pronounced R prime) is a counterclockwise right turn.
+     R2 is a 180 degree right turn.
+     In this way, any sequence of moves on the cube can be encoded as text.
+     */
+    public static final List<String> VALID_MOVES = Arrays.asList("R", "L", "F", "B", "U", "D",
+            "R'", "L'", "F'", "B'", "U'", "D'", "R2", "L2", "F2", "B2", "U2", "D2");
+    private List<String> currentScramble;
+    private Random rand;
+
+    // effects: constructs a new ScrambleGenerator
+    public ScrambleGenerator() {
+        currentScramble = new ArrayList<>();
+        rand = new Random();
+    }
+
+    // requires: scrambleLength > 0
+    // modifies: this
+    // effects: stores a sequence of random meaningful moves in currentScramble of length scrambleLength
+    public void generateScramble(int scrambleLength) {
+        currentScramble.clear();
+        String previousMove = " ";
+        String currentMove;
+        while (currentScramble.size() < scrambleLength) {
+            currentMove = VALID_MOVES.get(rand.nextInt(VALID_MOVES.size())); // Get a random move
+            if (currentMove.charAt(0) != previousMove.charAt(0)) { // Two subsequent moves should not have same letter
+                currentScramble.add(currentMove);
+                previousMove = currentMove;
+            }
+        }
+    }
+
+    // effects: returns a string representation of currentScramble
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        for (String move : currentScramble) {
+            output.append(" ").append(move);
+        }
+        return output.toString().trim(); // Trim to remove leading space
+    }
+
+    public List<String> getCurrentScramble() {
+        return currentScramble;
+    }
+}
